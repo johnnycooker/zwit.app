33835c4c0d42c398ee0dda0af8aca5f58dd03707
Johnny Cooker <johnnycooker2000@gmail.com>
2023-04-22 19:05:36 +0200

    v.0.78

--- components/layout/layout.tsx
+++ components/layout/layout.tsx
@@ -1,18 +1,13 @@
 import React, { useEffect, useState } from "react";
-import axios from "axios";
 import Navbar from "../navbar/navbar";
-import { url } from "inspector";
-
-const FirebaseUrl = "https://zwit-cba2d-default-rtdb.europe-west1.firebasedatabase.app/";
+import { fetchBackgroundImageUrl } from "@/pages/api/getBackground"; 
 
 interface CurrentUser {
   id: string;
 }
 
-
 function Layout(props: any) {
   const [backgroundImageUrl, setBackgroundImageUrl] = useState("https://www.national-geographic.pl/media/cache/slider_big/uploads/media/default/0014/43/zorza-polarna_2.jpeg");
-  
   const [currentUser, setCurrentUser] = useState<CurrentUser | null>(null);
 
   useEffect(() => {
@@ -25,24 +20,11 @@ function Layout(props: any) {
   }, []);
 
   useEffect(() => {
-    axios
-      .get(`${FirebaseUrl}/background/${currentUser?.id}/url.json`)
-      .then((response) => {
-        if (response.data) {
-          const url = response.data.data;
-          setBackgroundImageUrl(url);
-          if (url === 'ZABKA'){
-            setBackgroundImageUrl("/images/tlo_menu.webp");
-          }else if (url === 'RESET'){
-            setBackgroundImageUrl("https://www.national-geographic.pl/media/cache/slider_big/uploads/media/default/0014/43/zorza-polarna_2.jpeg");
-          }else if (url === '2137'){
-            setBackgroundImageUrl("https://s1.tvp.pl/images2/1/4/0/uid_140acb647481ecf84f8c1c32a6d6539c1652854105962_width_1920_play_0_pos_0_gs_0_height_1080.jpg");
-          }
-        }
-      })
-      .catch((error) => {
-        console.error("Error fetching background image URL:", error);
+    if (currentUser?.id) {
+      fetchBackgroundImageUrl(currentUser.id).then((url) => {
+        setBackgroundImageUrl(url);
       });
+    }
   }, [currentUser?.id]);
 
   const styles = {
