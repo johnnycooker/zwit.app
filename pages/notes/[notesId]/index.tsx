c9b8781d88c1e78ae5594f0bcc7f131b32ea2627
Johnny Cooker <johnnycooker2000@gmail.com>
2023-04-24 13:24:00 +0200

    v.0.92.1

--- pages/notes/[notesId]/index.tsx
+++ pages/notes/[notesId]/index.tsx
@@ -1,59 +1,29 @@
 import React, { useEffect, useState } from "react"
-import { NextPageContext } from "next"
 import { useRouter } from "next/router"
 import { getSession } from "next-auth/react"
 import NotesElement from "@/components/notes/notesId"
 import NotesLoadingPage from "@/components/notes/notesLoadingPage"
-
-const LoadingPage = () => {
-  return (
-    <>
-      <NotesLoadingPage />
-    </>
-    
-  )
-}
-
-export async function getServerSideProps(context: NextPageContext) {
-  const session = await getSession(context);
-
-  if (!session) {
-      return {
-          redirect: {
-              destination: '/auth',
-              permanent: false,
-          }
-      }
-  }
-
-  return {
-      props: {}
-  }
-}
 
 const NotesId = () => {
   const router = useRouter()
   const [loading, setLoading] = useState(true)
 
   useEffect(() => {
-    const handleRouteChange = () => {
-      setLoading(true)
-      setTimeout(() => {
-        setLoading(false)
-      }, 800)
+    const fetchData = async () => {
+      const session = await getSession();
+      if (!session) {
+        router.push('/auth');
+        return;
+      }
+      setLoading(false);
     };
 
-    router.events.on('routeChangeComplete', handleRouteChange)
-
-    return () => {
-      router.events.off('routeChangeComplete', handleRouteChange)
-    };
+    fetchData();
   }, [router])
 
   return (
     <>
-      {loading && <LoadingPage />}
-      {!loading && <NotesElement />}
+      {loading ? <NotesLoadingPage /> : <NotesElement />}
     </>
   )
 }
